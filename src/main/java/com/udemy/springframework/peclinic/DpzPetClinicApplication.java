0ebe2ce83b2f6940286a73261fe0a946bf6e8dce
anthony duprez <duprez.a@gmail.com>
2020-11-19 12:43:26 +0100

    First Commit

---
+++ src/main/java/com/udemy/springframework/peclinic/DpzPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.springframework.peclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpzPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpzPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udemy/springframework/peclinic/DpzPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.springframework.peclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpzPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
